4abea55bc6714a77b5494f11414a1006d06afc20
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-27 16:51:48 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,7 +8,7 @@ class PigLatinizer
   # transfer initial consonant of each word to the end of word and add vocalic syllable
   # example: Noodle Soup = oodlenay oupsay
   def latinize
-    
+    @string[1, -1]
 
   end
 
