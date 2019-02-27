46332dcaa00e8cd577074d7c3b69539cf5650224
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-27 17:07:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    @latinized_text = PigLatinizer.new(params[:user_text]).latinize
+    @latinized_text = PigLatinizer.new(params[:user_text])
     erb :results
   end
 
