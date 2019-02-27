4fb75f4c32e67f80ff7fb448572d81d1ce58d61d
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-27 17:23:11 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,19 +1,37 @@
-
 class PigLatinizer
 
-  def initialize(text)
-    @text= text.downcase
+  def piglatinize(input_str)
+    input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+  end
+
+  private
+
+  def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
+  end
+
+  def piglatinize_word(word)
+    # word starts with vowel
+    if !consonant?(word[0])
+      word = word + "w"
+    # word starts with 3 consonants
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+    # word starts with 2 consonants
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
+    # word starts with 1 consonant
+    else
+      word = word.slice(1..-1) + word.slice(0)
+    end
+    word << "ay"
+  end
+
+  def piglatinize_sentence(sentence)
+    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
   end
 
 
-  # transfer initial consonant of each word to the end of word and add vocalic syllable
-  # example: Noodle Soup = oodlenay oupsay
-  def piglatinize
-    init_consonant = @text[0]
-    piglatin_string = @text.slice(1.. -1) << init_consonant
-    piglatin_string << "ay"
-    piglatin_string
-  end
 
 
 end
